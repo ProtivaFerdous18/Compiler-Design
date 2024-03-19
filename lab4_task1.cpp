7b85a0db5b3095149654f80663cbeaf48192da7b
ProtivaFerdous18 <158180277+ProtivaFerdous18@users.noreply.github.com>
2024-03-19 19:56:00 +0600

    Add files via upload

---
+++ lab4_task1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include<fstream>
+using namespace std;
+
+bool isInputValidIdentifier(string &input)
+ {
+    if (!((input[0] >= 'a' && input[0] <= 'z') ||
+           (input[0] >= 'A' && input[0] <= 'Z') ||
+            input[0] == '_'))
+
+        return false;
+
+    for (int i = 1; i < input.length(); ++i)
+        {
+        if (!((input[i] >= 'a' && input[i] <= 'z') ||
+            (input[i] >= 'A' && input[i] <= 'Z') ||
+            (input[i] >= '0' && input[i] <= '9') ||
+             input[i] == '_'))
+            return false;
+    }
+    return true;
+}
+
+void outputCheck(const string& filename)
+ {
+
+    ifstream file(filename);
+
+
+    if (!file.is_open())
+        {
+        cerr << "Error " << filename << endl;
+        return;
+        }
+
+    string word;
+    while (file >> word)
+        {
+        if (isInputValidIdentifier(word))
+        {
+            cout << "The input '" << word << "' is valid." << endl;
+        } else {
+            cout << "The input '" << word << "' is not valid." << endl;
+        }
+    }
+
+    file.close();
+}
+
+int main()
+{
+
+   string filename;
+
+    cout << " Enter file : ";
+    getline(cin,filename);
+
+
+    outputCheck(filename);
+
+    return 0;
+}
